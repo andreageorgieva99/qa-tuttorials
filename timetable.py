8fa022c8b4d568d60ee8f52ce5dceb8cc7bbd959
andreageorgieva99 <116066607+andreageorgieva99@users.noreply.github.com>
2022-10-20 15:14:28 +0100

    timestable

---
+++ timetable.py
@@ -0,0 +1,8 @@
+number1 = 0
+while number1 <= 9 :
+    number1 = number1 +1
+    number2 = 0
+
+    while number2 <= 9:
+        number2=number2+1
+        print(number1, "x", number2  , "=", number1 *number2   )

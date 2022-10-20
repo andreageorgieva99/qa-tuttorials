21e7cb2a7be70f3bcbb33e993b4bef2a4f4321e5
andreageorgieva99 <116066607+andreageorgieva99@users.noreply.github.com>
2022-10-20 09:53:48 +0100

    boolean

---
+++ boolean.py
@@ -0,0 +1,7 @@
+number1 = input('Enter number1: ')
+number2 = input('Enter number2: ')
+int_num= int(number1)
+float_num = float(number2)
+round_num = (int(float_num))
+
+print(int_num, float_num, round_num)
